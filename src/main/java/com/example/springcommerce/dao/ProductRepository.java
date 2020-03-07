6e9edf598f53ae0e5b4cd68c62ab106fb70e31e0
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-07 15:06:43 +0100

    Add DAOs ProductCategoryRepository and ProductRepository

---
+++ src/main/java/com/example/springcommerce/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.springcommerce.dao;
+
+import com.example.springcommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
