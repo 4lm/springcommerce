0f6e5916e0616a32a527dad76534c18ac06091be
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-10 14:29:33 +0100

    Add CORS

--- src/main/java/com/example/springcommerce/dao/ProductRepository.java
+++ src/main/java/com/example/springcommerce/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.example.springcommerce.dao;
 
 import com.example.springcommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
