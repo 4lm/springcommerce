a8dccc0887704608ce21fd1de2e1bc5e2c7fbda3
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-19 13:57:58 +0100

    Improve naming

--- src/main/java/com/example/springcommerce/config/MyDataRestConfig.java
+++ src/main/java/com/example/springcommerce/config/MyDataRestConfig.java
@@ -13,19 +13,19 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+        HttpMethod[] forbiddenMethods = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(forbiddenMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(forbiddenMethods));
 
         // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(forbiddenMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(forbiddenMethods));
 
     }
 }
